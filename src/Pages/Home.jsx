e1cfc3a341932394c33dbdddc353c05229f122f8
Ebinesh <ebinesh1403@gmail.com>
2026-07-26 03:54:51 +0530

    fix: correct components directory casing

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,7 @@ import Contact from "../Components/Contact";
 import Facilities from "../Components/Facilities";
 import Footer from "../Components/Footer";
 import Gallery from "../Components/Gallery";
-import Hero from "../components/Hero";
+import Hero from "../Components/Hero";
 import Navbar from "../Components/Navbar";
 import Testimonials from "../Components/Testimonials";
 import WhyChoose from "../Components/Whychooseus";
